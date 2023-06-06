b13e2a59262dfa2f5d40f4e0d02e500aaecfb313
TWMingR <namova25@gmail.com>
2023-06-07 05:10:27 +0800

    feat: 🎸 modify vite config content

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    postcss()
+  ],
   base: '/timer/',
   resolve: {
     alias: {
