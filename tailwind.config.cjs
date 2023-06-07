8e14325a1ef005299a2b570e7fc2ab550b45b3de
TWMingR <namova25@gmail.com>
2023-06-07 16:14:25 +0800

    feat: 🎸 modify tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
