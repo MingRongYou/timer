2e71a92b12a05b978914d790f982efb5bebc6543
TWMingR <namova25@gmail.com>
2023-06-07 16:03:06 +0800

    feat: 🎸 modify postcss

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-    },
-  },
+
   plugins: [
     vue(),
   ],
