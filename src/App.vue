37938ffa7dadc472e94e36d28f9f2f81029fa63d
TWMingR <namova25@gmail.com>
2023-06-07 13:19:49 +0800

    feat: 🎸 modify timer.js content

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
 import { useTimerStore } from "@/stores/timer";
+import { storeToRefs } from "pinia";
 
-const timerStore = useTimerStore();
-
-function startTimer() {
-  timerStore.startTimer();
-}
-function stopTimer() {
-  timerStore.stopTimer();
-}
-function resetTimer() {
-  timerStore.resetTimer();
-}
+const store = useTimerStore();
+const { startTimer, stopTimer, resetTimer } = store;
+const { showButton, formattedTime } = storeToRefs(store);
 </script>
 
 <template>
@@ -21,29 +14,29 @@ function resetTimer() {
     </section>
     <section class="flex text-6xl justify-center content-center">
       <div class="mr-3 ml-3 mt-3 relative">
-        {{ timerStore.formattedTime.days }}
+        {{ formattedTime.days }}
         <div class="label ml-4 text-sm absolute bottom-0">days</div>
       </div>
       <span class="leading-snug">:</span>
       <div class="mr-3 ml-3 mt-3 relative">
-        {{ timerStore.formattedTime.hours }}
+        {{ formattedTime.hours }}
         <div class="label ml-3 text-sm absolute bottom-0">hours</div>
       </div>
       <span class="leading-snug">:</span>
       <div class="mr-3 ml-3 mt-3 relative">
-        {{ timerStore.formattedTime.minutes }}
+        {{ formattedTime.minutes }}
         <div class="label ml-1 text-sm absolute bottom-0">minutes</div>
       </div>
       <span class="leading-snug">:</span>
       <div class="mr-3 ml-3 mt-3 relative">
-        {{ timerStore.formattedTime.seconds }}
+        {{ formattedTime.seconds }}
         <div class="label ml-1 text-sm absolute bottom-0">seconds</div>
       </div>
     </section>
     <section>
       <button
         class="block uppercase mx-auto w-30 h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded mt-5"
-        v-if="timerStore.getShowButton"
+        v-if="showButton"
         @click="startTimer"
       >
         Start
--- src/stores/timer.js
+++ src/stores/timer.js
@@ -1,44 +1,39 @@
 import { defineStore } from 'pinia'
+import {computed, ref} from 'vue'
 
-export const useTimerStore = defineStore('timer', {
-  state: () => ({
-    time: 0,
-    isRunning: false,
-    intervalId :null,
-    showButton : true
-  }),
+export const useTimerStore = defineStore('timer', () => {
+    const time = ref(0)
+    const isRunning = ref(false)
+    const intervalId = ref(null)
+    const showButton = ref(true)
 
-  getters: {
-    formattedTime() {
-      const days = String(Math.floor(this.time / 60 / 60 / 24)).padStart(2, '0');
-      const hours = String(Math.floor(this.time / 60 / 60) % 24).padStart(2, '0');
-      const minutes = String(Math.floor(this.time / 60) % 60).padStart(2, '0');
-      const seconds = String(this.time % 60).padStart(2, '0');
-      return {days,hours,minutes,seconds}
-    },
-    getShowButton() {
-        return this.showButton;
-      }
-  },
+    const formattedTime = computed(()=>{
+        const days = String(Math.floor(time.value / 60 / 60 / 24)).padStart(2, '0');
+        const hours = String(Math.floor(time.value / 60 / 60) % 24).padStart(2, '0');
+        const minutes = String(Math.floor(time.value / 60) % 60).padStart(2, '0');
+        const seconds = String(time.value % 60).padStart(2, '0');
+        return {days,hours,minutes,seconds}
+    })
 
-  actions: {
-    startTimer() {
-      this.showButton = false
-      this.isRunning = true
-      this.intervalId = setInterval(() => {
-        this.time++
-      }, 1000)
-    },
-    stopTimer() {
-        this.showButton = true
-        this.isRunning = false
-        clearInterval(this.intervalId);
-      },
-    resetTimer(){
-        this.showButton = true
-        this.isRunning = false
-        this.time = 0;
-        clearInterval(this.intervalId);
+    const startTimer = () => {
+        showButton.value = false
+        isRunning.value = true
+        intervalId.value = setInterval(() => {
+        time.value++
+        }, 1000)
     }
-  },
+
+    const stopTimer = () => {
+        showButton.value = true
+        isRunning.value = false
+        clearInterval(intervalId.value);
+    }
+    const resetTimer = () => {
+        showButton.value = true
+        isRunning.value = false
+        time.value = 0;
+        clearInterval(intervalId.value);
+    }
+
+    return {showButton, formattedTime, startTimer, stopTimer, resetTimer}
 })
