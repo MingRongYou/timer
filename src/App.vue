354a2f7632526c14b858e2f7b66fadc79f405d0d
TWMingR <namova25@gmail.com>
2023-06-12 22:53:08 +0800

    feat: 🎸 delete font-design and add base-rwd

--- src/App.vue
+++ src/App.vue
@@ -9,47 +9,49 @@ const { showButton, formattedTime } = storeToRefs(store);
 
 <template>
   <div>
-    <section class="h-40 flex justify-center items-end">
-      <div class="text-3xl">Timer demo</div>
+    <section class="h-10 lg:h-40 flex justify-center items-end">
+      <div class="lg:text-3xl text-xl">Timer demo</div>
     </section>
-    <section class="flex text-6xl justify-center content-center">
-      <div class="mr-3 ml-3 mt-3 relative">
+    <section
+      class="flex text-3xl lg:text-6xl justify-center content-center place-content-center"
+    >
+      <div class="mr-2 ml-2 mt-2 lg:mr-3 lg:ml-3 lg:mt-3 relative">
         {{ formattedTime.days }}
-        <div class="label ml-4 text-sm absolute bottom-0">days</div>
+        <div class="label lg:text-sm text-xs left-2 absolute my-1">days</div>
       </div>
       <span class="leading-snug">:</span>
-      <div class="mr-3 ml-3 mt-3 relative">
+      <div class="mr-2 ml-2 mt-2 lg:mr-3 lg:ml-3 lg:mt-3 relative">
         {{ formattedTime.hours }}
-        <div class="label ml-3 text-sm absolute bottom-0">hours</div>
+        <div class="label lg:text-sm text-xs left-2 absolute my-1">hours</div>
       </div>
       <span class="leading-snug">:</span>
-      <div class="mr-3 ml-3 mt-3 relative">
+      <div class="mr-2 ml-2 mt-2 lg:mr-3 lg:ml-3 lg:mt-3 relative">
         {{ formattedTime.minutes }}
-        <div class="label ml-1 text-sm absolute bottom-0">minutes</div>
+        <div class="label lg:text-sm text-xs left-2 absolute my-1">minutes</div>
       </div>
       <span class="leading-snug">:</span>
-      <div class="mr-3 ml-3 mt-3 relative">
+      <div class="mr-2 ml-2 mt-2 lg:mr-3 lg:ml-3 lg:mt-3 relative">
         {{ formattedTime.seconds }}
-        <div class="label ml-1 text-sm absolute bottom-0">seconds</div>
+        <div class="label lg:text-sm text-xs left-2 absolute my-1">seconds</div>
       </div>
     </section>
-    <section>
+    <section class="mt-10 w-">
       <button
-        class="block uppercase mx-auto w-30 h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded mt-5"
+        class="block uppercase mx-auto lg:w-30 lg:h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs lg:py-3 lg:px-10 py-1 px-5 rounded mt-5"
         v-if="showButton"
         @click="startTimer"
       >
         Start
       </button>
       <button
-        class="block uppercase mx-auto w-30 h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded mt-5"
+        class="block uppercase mx-auto lg:w-30 lg:h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs lg:py-3 lg:px-10 py-1 px-5 rounded mt-5"
         v-else
         @click="stopTimer"
       >
         Stop
       </button>
       <button
-        class="block uppercase mx-auto w-30 h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded mt-5"
+        class="block uppercase mx-auto lg:w-30 lg:h-15 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs lg:py-3 lg:px-10 py-1 px-5 rounded mt-5"
         @click="resetTimer"
       >
         Reset
